7edf092a46319fd05f7e02a5f0dd41a5d553d1f2
HieuLN5 <hieutroll99@gmail.com>
2022-10-02 17:32:36 +0700

    change

---
+++ QuizzApp/QuizzApp/QuizzApp.Data/Configuration/TestExamConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuizzApp.Data.Entities;
+
+namespace QuizzApp.Data.Configuration
+{
+    internal class TestExamConfiguration : IEntityTypeConfiguration<TestExam>
+    {
+        public void Configure(EntityTypeBuilder<TestExam> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.HasOne(te => te.Course)
+               .WithMany(c => c.Tests)
+               .HasForeignKey(te => te.CourseId)
+               .IsRequired(false)
+               .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
