a49c287472dbd40c30ec4a2ebe30f5914448fa3c
HieuLN5 <hieutroll99@gmail.com>
2022-11-27 18:48:47 +0700

    hieuln5

---
+++ QuizzApp/QuizzApp/Models/Questions/QuestionEditVM.cs
@@ -0,0 +1,19 @@
+﻿using QuizzApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace QuizzApp.VModels.Questions
+{
+    public class QuestionEditVM
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Question")]
+        public string QuestionName { get; set; }
+
+        [DisplayName("Course")]
+        public virtual Course Course { get; set; }
+        public virtual IList<Option> Options { get; set; }
+    }
+}
